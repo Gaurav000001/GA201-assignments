a8afedd07ae1ab5b2a8eb34ae9e54ee672a6fbe7
Gaurav <gangulygaurav166@gmail.com>
2023-08-05 10:50:03 +0530

    frontend for code converter

--- Sprint-1 Day-2 Code Convertor/Code Converter Frontend/index.js
+++ Sprint-1 Day-2 Code Convertor/Code Converter Frontend/index.js
@@ -10,6 +10,7 @@ function check(){
     else if (l === "click") e.setOption("mode", "text/x-java");
     else if (l === "python") e.setOption("mode", "python");
     else if (l === "text/x-c++src") e.setOption("mode", "text/x-c++src");
+    // console.log(e.getValue());
 
     // Refresh the CodeMirror instance to update the mode
     e.refresh();
@@ -29,5 +30,31 @@ function ChangeLanguage(language){
         lineWrapping: true, // Enable line wrapping
         readOnly: false, // Set to true to make the editor read-only
         autoCloseBrackets: true, // Automatically close brackets
+    });
+}
+
+async function run(){
+    let code = e.getValue();
+    let language = document.getElementById('dropdown2').value;
+    let output = document.getElementById('response-container');
+    console.log(code);
+
+
+    let response = await fetch(`http://localhost:9090/convert.in/run`, {
+        method: "POST",
+        headers: {
+        'Content-Type': 'text/plain',
+        },
+        body: code
+    })
+    .then(response => response.json())
+    .then(result => {
+
+        output.innerText = result;
+
+    })
+    .catch(error => {
+        output.textContent = "Server down please try again later...";
+        output.style.color = "red";
     });
 }
